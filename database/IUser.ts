0a5890463f598504a2870ec062c8c44fe58ea240
deimonbody <pavlov.d@donnu.edu.ua>
2023-12-22 16:26:08 +0200

    commit edit mongodb connection

---
+++ database/IUser.ts
@@ -0,0 +1,16 @@
+import { Schema, Document } from "mongoose";
+
+export interface IUser extends Document {
+  clerkId: string;
+  name: string;
+  username: string;
+  email: string;
+  password?: string;
+  bio?: string;
+  picture: string;
+  location?: string;
+  portfolioWebsite?: string;
+  reputation?: number;
+  saved: Schema.Types.ObjectId[];
+  joinedAt: Date;
+}
