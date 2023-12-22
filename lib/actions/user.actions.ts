0337d4b5e659c71c7b99b7edb77792dbf8aa540e
deimonbody <pavlov.d@donnu.edu.ua>
2023-12-22 09:57:23 +0200

    Second try

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -21,50 +21,64 @@ export async function getUserById(params: any) {
     throw err;
   }
 }
-
 export async function createUser(userData: CreateUserParams) {
   try {
     connectToDatebase();
+
     const newUser = await User.create(userData);
+
     return newUser;
-  } catch (err) {
-    console.log(err);
-    throw err;
+  } catch (error) {
+    console.log(error);
+    throw error;
   }
 }
 
 export async function updateUser(params: UpdateUserParams) {
   try {
     connectToDatebase();
+
     const { clerkId, updateData, path } = params;
 
-    await User.findOneAndUpdate({ clerkId }, updateData, { new: true });
+    await User.findOneAndUpdate({ clerkId }, updateData, {
+      new: true,
+    });
 
     revalidatePath(path);
-  } catch (err) {
-    console.log(err);
-    throw err;
+  } catch (error) {
+    console.log(error);
+    throw error;
   }
 }
 
 export async function deleteUser(params: DeleteUserParams) {
   try {
     connectToDatebase();
+
     const { clerkId } = params;
 
     const user = await User.findOneAndDelete({ clerkId });
-    if (!user) throw new Error("User not found");
 
-    // const userQuestionIds = await Question.find({ author: user._id }).distinct(
-    //   "_id"
-    // );
+    if (!user) {
+      throw new Error("User not found");
+    }
 
+    // Delete user from database
+    // and questions, answers, comments, etc.
+
+    // get user question ids
+    // const userQuestionIds = await Question.find({ author: user._id}).distinct('_id');
+
+    // delete user questions
     await Question.deleteMany({ author: user._id });
 
+    // TODO: delete user answers, comments, etc.
+
     const deletedUser = await User.findByIdAndDelete(user._id);
+
     return deletedUser;
-  } catch (err) {
-    console.log(err);
-    throw err;
+  } catch (error) {
+    console.log(error);
+    throw error;
   }
 }
--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -4,19 +4,24 @@ let isContected: boolean = false;
 
 export const connectToDatebase = async () => {
   mongoose.set("strictQuery", true);
+
   if (!process.env.MONGODB_URL) {
-    return console.log("Missing DB url");
+    return console.log("MISSING MONGODB_URL");
   }
+
   if (isContected) {
-    console.log("mongoDB is already connected");
     return;
   }
 
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
-      dbName: "devflowNextjs",
+      dbName: "devflow",
     });
+
     isContected = true;
+
     console.log("MongoDB is connected");
-  } catch (error) {}
+  } catch (error) {
+    console.log("MongoDB connection failed", error);
+  }
 };
